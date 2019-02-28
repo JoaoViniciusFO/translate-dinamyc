4ebe6ae74d86bf02d0a58b3b4d270ef265dba29a
JOÃO VINICIUS FERNANDES OLIVEIRA <x03355258148@tcu.gov.br>
2019-02-28 10:10:25 -0300

    teste do jenkins

--- src/pages/pipe/pipe-translate.ts
+++ src/pages/pipe/pipe-translate.ts
@@ -19,7 +19,7 @@ export class PipeTranslate implements PipeTransform {
     transform(obj): any{
         let arrPath = obj.split(".");
         let db = new PouchDB('translate');
-
+        console.log("nao funfa")
         return this.traducao$;
         
         // return db.allDocs({
